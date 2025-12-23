d2755d44313f6baa17a737add1d07250bc4f362b
Lionel <lionel.gweshe@yahoo.com>
2025-12-23 21:51:27 +0000

    Adjust paths

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,18 @@
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.env.NODE_ENV === 'development';
+
 export default {
-  kit: {
-    adapter: adapter({
-      strict: true
-    }),
-
-    paths: {
-      base: '/Eventus-Estates'
-      // ⚠️ DO NOT set `assets`
-    },
-
-    prerender: {
-      entries: ['*']
-    }
-  }
+	kit: {
+		adapter: adapter({
+			fallback: 'index.html'
+		}),
+		paths: {
+			base: '/Eventus-Estates',
+			assets: '/Eventus-Estates'
+		},
+		prerender: {
+			entries: ['*']
+		}
+	}
 };
